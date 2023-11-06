ac1276a89142647d878f86f5e54ea96cf44eff8d
forLoop94 <charlesuwem94@gmail.com>
2023-11-06 16:06:00 +0100

    Post sign up data from client to server to be persisted in the database

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import styles from '../styles/root.module.css';
 
 export const Root = () => {
   return (
     <>
       <h1>Root</h1>
-      <Link to='/login'>Log in</Link>
-      <Link to='/signup'>Sign up</Link>
+      <nav>
+        <Link to='/login'>Log in</Link>
+        <Link to='/signup'>Sign up</Link>
+      </nav>
     </>
   )
 }
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,7 +1,83 @@
 import React from 'react'
+import { useState } from 'react'
 
 export const Signup = () => {
+  const [error, setError] = useState('');
+  const [success, setSuccess] = useState('');
+  const [data, setData] = useState({
+    "email": '',
+    "password": '',
+    "password_confirmation": ''
+  });
+
+  const baseURL = 'http://localhost:4000';
+
+
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+    console.log(data);
+    try {
+      const response = await fetch(`${baseURL}/signup`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          user: data
+        })
+      })
+      if(response.ok) {
+        const responseData = await response.json();
+        console.log(responseData);
+        const authorization = response.headers.get('authorization');
+        console.log(authorization);
+        setSuccess(responseData.status.message);
+      } else {
+        const errorData = await response.json();
+        setError(errorData.status.message);
+      }
+
+    } catch(error) {
+      setError(error.message);
+      console.log(response.status.message)
+    }
+  }
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setData({
+      ...data,
+      [name]: value
+    })
+
+  }
+
   return (
-    <div>Signup</div>
+    <form onSubmit={handleSubmit}>
+      <input
+        type="email"
+        placeholder="email"
+        value={data.email}
+        name="email"
+        onChange={handleChange}
+      />
+      <input
+        type="password"
+        placeholder="password"
+        value={data.password}
+        name="password"
+        onChange={handleChange}
+      />
+      <input
+        type="password"
+        placeholder="password confirmation"
+        value={data.password_confirmation}
+        name="password_confirmation"
+        onChange={handleChange}
+      />
+      <button type='submit'>Submit</button>
+      <p>{success}</p>
+      <p>{error}</p>
+    </form>
   )
 }
