103974ba4b83c5ed3ba91a230e881c24b3632f25
forLoop94 <charlesuwem94@gmail.com>
2023-11-06 17:10:05 +0100

    Post login data to server

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,63 @@
 import React from 'react'
+import { useState } from 'react'
 
 export const Login = () => {
+  const [data, setData] = useState({
+    'email': '',
+    'password': ''
+  })
+
+  const baseURL = "http://localhost:4000";
+
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+    try {
+      const res = await fetch(`${baseURL}/login`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          user: data
+        })
+      })
+      if(res.ok) {
+        const resData = await res.json()
+        console.log(resData);
+        const authorization = response.headers.get('authorization');
+        localStorage.setItem('token', JSON.stringify(authorization));
+        console.log(resData.message)
+      } else {
+        const resData = await res.json()
+        console.log(resData.message);
+      }
+    } catch(error) {
+      return error;
+    }
+  }
+
+  const handleChange = (e) => {
+    setData({
+      ...data,
+      [e.target.name]: e.target.value
+    })
+  }
+
   return (
-    <div>Login</div>
+    <form onSubmit={handleSubmit}>
+      <input
+        type='email'
+        placeholder='email'
+        name='email'
+        onChange={handleChange}
+      />
+      <input
+        type='password'
+        placeholder='password'
+        name='password'
+        onChange={handleChange}
+      />
+      <button type='submit'>Submit</button>
+    </form>
   )
 }
